481466905b2946bb4f3590e46a9e3f0436be83b9
Hardik Rathod <hardikkrathod@gmail.com>
2023-08-25 11:48:31 +0530

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+/* eslint-disable no-mixed-operators */
+
+import Lottie from 'lottie-react'
+import four2 from './assest/four.json';
+import { useRef } from 'react';
+import './App.css';
+
+function App() {
+  const notFoundRef = useRef()
+  return (
+    <>
+      <div className="fourHeader">
+        <div className='four'>
+      <div className='pageTitle'>The Page not found. Click here to go to <a href='/' className='fourLink'>Home</a></div>
+        <Lottie 
+        lottieRef={notFoundRef} animationData={four2}
+        />
+      </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
